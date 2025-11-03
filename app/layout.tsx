ae1c2cc67222e36e6236df62cbcb397413b8f63e
Gregory Ajayi <gregory@lagoslabs.dev>
2025-11-02 19:48:13 -0600

    update to cart

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import NextTopLoader from 'nextjs-toploader';
 import './globals.css';
 
 
+//
+
 export const metadata: Metadata = {
   title: 'Mehchant-Admin',
   description: 'Mehchant-Admin Store Dashboard'
