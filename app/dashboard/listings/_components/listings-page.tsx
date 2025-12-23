bf47d4c850bfa85939e76a081d852e75276dca6f
femi <cocofemi@gmail.com>
2025-12-24 00:34:11 +0100

    changes

--- app/dashboard/listings/_components/listings-page.tsx
+++ app/dashboard/listings/_components/listings-page.tsx
@@ -66,7 +66,7 @@ export default function ListingsPage({}: TUserListingPage) {
       <div className="space-y-4">
         <div className="flex items-start justify-between">
           <Heading title={`Listings (${totalListings})`} description="" />
-          {user?.role === 'store' && !user?.stripeOnboardingComplete && (
+          {user?.role === 'store' && !user?.stripePayoutsEnabled && (
             <Link
               href={'/dashboard/listings/create'}
               className={cn(buttonVariants({ variant: 'default' }))}
